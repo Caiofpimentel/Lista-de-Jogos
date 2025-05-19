bf798b62903658980e8eece42b1bd52f8eed3abb
Caio Pimentel <caio.pimentel@sis-it.com>
2025-05-19 19:44:18 -0300

    Game seed

--- src/main/java/com/caiofpimentel/ListaDeJogos/entities/Game.java
+++ src/main/java/com/caiofpimentel/ListaDeJogos/entities/Game.java
@@ -1,9 +1,17 @@
 package com.caiofpimentel.ListaDeJogos.entities;
 
-import jakarta.persistence.*;
-import lombok.*;
-
 import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -21,10 +29,14 @@ public class Game {
     private Integer year;
 
     private String genre;
-    private String platform;
+    private String platforms;
     private Double score;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     @Override
