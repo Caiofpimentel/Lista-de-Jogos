0aeb5e089fed80f814585e068a4dcaf7898e05c8
Caio Pimentel <caio.pimentel@sis-it.com>
2025-05-20 18:33:50 -0300

    Domain model

---
+++ src/main/java/com/caiofpimentel/ListaDeJogos/entities/Belonging.java
@@ -0,0 +1,41 @@
+package com.caiofpimentel.ListaDeJogos.entities;
+
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "tb_beloging")
+@Getter
+@Setter
+@EqualsAndHashCode
+public class Belonging {
+
+    @EmbeddedId
+    private BelongingPK id = new BelongingPK();
+    
+    @EqualsAndHashCode.Exclude
+    private Integer position;
+
+    public Belonging(){
+
+    }
+
+    public Belonging(Game game, GameList list, Integer position ){
+
+        id.setGame(game);
+        id.setList(list);
+        this.position = position;
+
+    }
+    
+
+
+
+
+}
